69100633c2352ab3609b97b34932a7ffc1a500b1
infil505 <darwin.silva.gadea@est.una.ac.cr>
2025-08-30 20:51:30 -0600

    mejoras en la parte de rutas, en la parte del controlador de usuarios

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 
-
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/me', [AuthController::class, 'me']);
         Route::post('/logout', [AuthController::class, 'logout']);
